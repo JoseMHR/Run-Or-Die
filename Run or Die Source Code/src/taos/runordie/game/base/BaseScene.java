c01465bdaecfecdd8ec860dae23b9bff2689a4c3
Jose Manuel Heredia Reyes <Jose.manuel.reyes.1986@gmail.com>
2015-02-13 11:03:28 +0100

    Upload Source Code

---
+++ Run or Die Source Code/src/taos/runordie/game/base/BaseScene.java
@@ -0,0 +1,37 @@
+package taos.runordie.game.base;
+
+import org.andengine.engine.Engine;
+import org.andengine.engine.camera.BoundCamera;
+import org.andengine.entity.scene.Scene;
+import org.andengine.opengl.vbo.VertexBufferObjectManager;
+
+import taos.runordie.game.manager.ResourcesManager;
+import taos.runordie.game.manager.SceneManager.SceneType;
+import android.app.Activity;
+
+public abstract class BaseScene extends Scene {
+
+	protected Engine engine;
+	protected Activity activity;
+	protected ResourcesManager resourcesManager;
+	protected VertexBufferObjectManager vbom;
+	protected BoundCamera camera;
+
+
+	public BaseScene() {
+		this.resourcesManager = ResourcesManager.getInstance();
+		this.engine = resourcesManager.engine;
+		this.activity = resourcesManager.activity;
+		this.vbom = resourcesManager.vbom;
+		this.camera = resourcesManager.camera;
+		createScene();
+	}
+
+	public abstract void createScene();
+
+	public abstract void onBackKeyPressed();
+
+	public abstract SceneType getSceneType();
+
+	public abstract void disposeScene();
+}
---
+++ Run or Die Source Code/src/taos/runordie/game/manager/SceneManager.java
@@ -0,0 +1,116 @@
+package taos.runordie.game.manager;
+
+import org.andengine.engine.Engine;
+import org.andengine.engine.handler.timer.ITimerCallback;
+import org.andengine.engine.handler.timer.TimerHandler;
+import org.andengine.ui.IGameInterface.OnCreateSceneCallback;
+
+import taos.runordie.game.base.BaseScene;
+import taos.runordie.game.scene.GameScene;
+import taos.runordie.game.scene.LoadingScene;
+import taos.runordie.game.scene.MainMenuScene;
+import taos.runordie.game.scene.SplashScene;
+
+public class SceneManager {
+
+	private BaseScene splashScene;
+	private BaseScene menuScene;
+	private BaseScene gameScene;
+	private BaseScene loadingScene;
+
+
+	private static final SceneManager INSTANCE = new SceneManager();
+	private SceneType currentSceneType = SceneType.SCENE_SPLASH;
+	private BaseScene currentScene;
+	private Engine engine = ResourcesManager.getInstance().engine;
+
+	public enum SceneType {
+		SCENE_SPLASH, SCENE_MENU, SCENE_GAME, SCENE_LOADING,
+	}
+
+	public void setScene(BaseScene scene) {
+		engine.setScene(scene);
+		currentScene = scene;
+		currentSceneType = scene.getSceneType();
+	}
+
+	public void setScene(SceneType sceneType) {
+		switch (sceneType) {
+		case SCENE_MENU:
+			setScene(menuScene);
+			break;
+		case SCENE_GAME:
+			setScene(gameScene);
+			break;
+		case SCENE_SPLASH:
+			setScene(splashScene);
+			break;
+		case SCENE_LOADING:
+			setScene(loadingScene);
+			break;
+		default:
+			break;
+		}
+	}
+
+	public void createMenuScene() {
+		ResourcesManager.getInstance().loadMenuResources();
+		menuScene = new MainMenuScene();
+		loadingScene = new LoadingScene();
+		SceneManager.getInstance().setScene(menuScene);
+		disposeSplashScene();
+	}
+
+	public void createSplashScene(OnCreateSceneCallback pOnCreateSceneCallback) {
+		ResourcesManager.getInstance().loadSplashScreen();
+		splashScene = new SplashScene();
+		currentScene = splashScene;
+		pOnCreateSceneCallback.onCreateSceneFinished(splashScene);
+	}
+
+	private void disposeSplashScene() {
+		ResourcesManager.getInstance().unloadSplashScreen();
+		splashScene.disposeScene();
+		splashScene = null;
+	}
+
+	public void loadGameScene(final Engine mEngine) {
+		setScene(loadingScene);
+		ResourcesManager.getInstance().unloadMenuTextures();
+		mEngine.registerUpdateHandler(new TimerHandler(0.1f,
+				new ITimerCallback() {
+					public void onTimePassed(final TimerHandler pTimerHandler) {
+						mEngine.unregisterUpdateHandler(pTimerHandler);
+						ResourcesManager.getInstance().loadGameResources();
+						gameScene = new GameScene();
+						setScene(gameScene);
+					}
+				}));
+	}
+
+	public void loadMenuScene(final Engine mEngine) {
+		setScene(loadingScene);
+		gameScene.disposeScene();
+		ResourcesManager.getInstance().unloadGameTextures();
+		mEngine.registerUpdateHandler(new TimerHandler(0.1f,
+				new ITimerCallback() {
+					public void onTimePassed(final TimerHandler pTimerHandler) {
+						mEngine.unregisterUpdateHandler(pTimerHandler);
+						ResourcesManager.getInstance().loadMenuTextures();
+						setScene(menuScene);
+					}
+				}));
+	}
+
+	public static SceneManager getInstance() {
+		return INSTANCE;
+	}
+
+	public SceneType getCurrentSceneType() {
+		return currentSceneType;
+	}
+
+	public BaseScene getCurrentScene() {
+		return currentScene;
+	}
+}
